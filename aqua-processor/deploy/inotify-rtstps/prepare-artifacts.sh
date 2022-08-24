5694b63d40f35eaa12abb68234d67d909f01a13c
Steven Schneider <stschn@microsoft.com>
2022-08-24 15:43:18 -0700

    Adding the inotify rt-stps tooling for processing data as it lands on the VM

---
+++ aqua-processor/deploy/inotify-rtstps/prepare-artifacts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright (c) 2022 Microsoft Corporation. All rights reserved.
+# Software is licensed under the MIT License. See LICENSE in the project
+# root for license information.
+
+set -eo pipefail
+
+WORKING_DIR="$(dirname "$0")"
+ARTIFACTS_DIR="${WORKING_DIR}/../artifacts/linux-x64/inotify-rtstps"
+mkdir -p ${ARTIFACTS_DIR}
+
+# Stage other scripts in artifacts folder to be copied over
+cp ${WORKING_DIR}/INotifyRTSTPS.service ${ARTIFACTS_DIR}
+cp ${WORKING_DIR}/inotify-rtstps.sh ${ARTIFACTS_DIR}
+
+tar -czvf ${ARTIFACTS_DIR}/inotify-rtstps-artifacts.tar.gz -C ${ARTIFACTS_DIR} .
