2aea88eb7cd2242279261e3879b2875dea694d6b
Micholas <10660866+micholas@users.noreply.github.com>
2022-08-25 14:15:53 -0400

    Create contact profile during deployment (#58)
    * Create contact profile during deployment.
    * Top level compile before build.

---
+++ tcp-to-blob/src/makeContactProfile.ts
@@ -0,0 +1,27 @@
+import { makeOrbitalHelper } from 'arm-orbital-helper/dist/orbitalHelper'
+import { getEnvVar } from './utils'
+import { MakeAquaContactProfileParams } from 'arm-orbital-helper/dist/aquaContactProfileParams'
+
+const makeContactProfile = async () => {
+    const { createContactProfile } = await makeOrbitalHelper()
+    const aksName = getEnvVar('AKS_NAME')
+    const params: MakeAquaContactProfileParams = {
+        name: `${aksName}-cp`,
+        endpointIP: getEnvVar('LB_IP'),
+        endpointPort: +getEnvVar('PORT'),
+        endpointName: aksName,
+        subnetId: `${getEnvVar('AKS_VNET_ID')}/subnets/orbital-subnet`,
+    }
+    console.log(
+        'Making contact profile. params:',
+        JSON.stringify(params, null, 2)
+    )
+    const res = await createContactProfile(params)
+    console.log('Created contact profile:', JSON.stringify(res, null, 2))
+}
+
+if (require.main === module) {
+    makeContactProfile().catch((error) => {
+        console.error('Failure creating contact profile:', error.message)
+    })
+}
