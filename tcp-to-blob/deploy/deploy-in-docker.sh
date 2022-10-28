e2c737463cd7558015499ccc7794a2d47fec7a33
micholas <10660866+micholas@users.noreply.github.com>
2022-10-27 23:38:07 -0400

    Updated deploy-in-docker.sh

--- tcp-to-blob/deploy/deploy-in-docker.sh
+++ tcp-to-blob/deploy/deploy-in-docker.sh
@@ -3,11 +3,14 @@
 # Software is licensed under the MIT License. See LICENSE in the project
 # root for license information.
 
-PROJECT_DIR="$(dirname -- "$0")/.."
+PROJECT_DIR="$(dirname -- "$0")/../.."
 echo "PROJECT_DIR: \"${PROJECT_DIR}\""
 pushd "${PROJECT_DIR}"
 
 mkdir tcp-to-blob/.env
 envsubst < tcp-to-blob/deploy/env-template.sh > tcp-to-blob/.env/env-template.sh
+
+. tcp-to-blob/.env/env-template.sh
+tcp-to-blob/deploy/bicep/deploy.sh
 
 popd
