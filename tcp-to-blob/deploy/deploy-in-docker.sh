71e4de1a589f9366382395664afa8603ea8a5a74
micholas <10660866+micholas@users.noreply.github.com>
2022-11-03 16:05:51 -0400

    Updated deploy-in-docker.sh

--- tcp-to-blob/deploy/deploy-in-docker.sh
+++ tcp-to-blob/deploy/deploy-in-docker.sh
@@ -11,11 +11,6 @@ echo "PROJECT_DIR: \"${PROJECT_DIR}\""
 pushd "${PROJECT_DIR}"
 ./install-node-modules.sh
 npx yarn build
-mkdir tcp-to-blob/.env
-echo "Creating \"tcp-to-blob/.env/env-template.sh\""
-echo "  • NAME_PREFIX=\"${NAME_PREFIX}\""
-envsubst < tcp-to-blob/deploy/env-template.sh > tcp-to-blob/.env/env-template.sh
-
 . tcp-to-blob/.env/env-template.sh
 tcp-to-blob/deploy/bicep/deploy.sh
 
