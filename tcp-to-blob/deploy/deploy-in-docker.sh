d645ce84b78422c1c8c27519a183aee4f40fa242
Micholas <10660866+micholas@users.noreply.github.com>
2022-12-01 15:25:02 -0500

    Create docker image for deploying TCP to BLOB (#176)
    * Created deployer base docker image
    * Created deploy-in-docker.sh and install node modules in deploy-in-docker.sh
    * Check if az cli logged in when starting deployment
    * Removed Dockerfile_deployer_tcp-to-blob
    * Check for env file and fail fast
    * Address Typescript @types/node [issue](https://github.com/microsoft/TypeScript/issues/51567).
    * Added default location
    
    Co-authored-by: Steven Schneider <stschn@microsoft.com>
    
    
    
    
    
    * Added @types/node as dev dependency
    
    Co-authored-by: Steven Schneider <stschn@microsoft.com>

---
+++ tcp-to-blob/deploy/deploy-in-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) 2022 Microsoft Corporation. All rights reserved.
+# Software is licensed under the MIT License. See LICENSE in the project
+# root for license information.
+
+set -euo pipefail
+
+. tcp-to-blob/.env/env-template.sh || (echo "Open a new shell window and run 'docker cp ./tcp-to-blob/.env/ t2b-deploy:/home/azure-orbital-integration/tcp-to-blob/'." && exit 1)
+az account show # Fail fast if not logged in.
+PROJECT_DIR="$(dirname -- "$0")/../.."
+echo "PROJECT_DIR: \"${PROJECT_DIR}\""
+pushd "${PROJECT_DIR}"
+./install-node-modules.sh
+npx yarn build
+tcp-to-blob/deploy/bicep/deploy.sh
+
+popd
